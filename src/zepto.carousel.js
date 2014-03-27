9619773ccfb2f13d48f821da8ad821cd7b376afc
yanhaijing <yanhaijing@yeah.net>
2014-03-27 12:02:58 +0800

    update
    1.add demo file
    2.添加多种样式支持
    3.添加动态设置步长 深度 旋转角度
    4.添加可设置阻止屏幕尺寸变更事件和阻止触摸事件

--- src/zepto.carousel.js
+++ src/zepto.carousel.js
@@ -7,81 +7,110 @@
 	$.fn.carousel = function(opt){
 		var
 			o = $.extend({
-				startIndex:1,
+				startIndex: 0,
 				li: "li",
 				visibleCount: 3,
-				step:0.2
-			}, opt),
-			$this = $(this),
-			$carousel = $this,
-			$lis = $(o.li, $this),
-			lisHeight = $lis.height(),
-			lisLength = $lis.length,
-			curIndex = o.startIndex,
-			width = $carousel.width();
-		
-		function fixIndex(index) {
-			return (index + lisLength) % lisLength;
-		}
-		function translate(index) {
-			var
-				len = o.visibleCount / 2,
-				i = 1,
-				leftIndex = 0,
-				rightIndex = 0;
-			index = fixIndex(index);
-			$lis.removeClass("js-carousel-li-visible").eq(index).addClass("js-carousel-li-visible js-carousel-li-cur")
-				.css({
-					"-webkit-transform": "none",
-					"z-index": 1000
-				});
-			
-			for(i; i <= len; i++) {
-				leftIndex = fixIndex(index - i);
-				rightIndex = fixIndex(index + i);
-				$lis.eq(leftIndex).addClass("js-carousel-li-visible").css({
-					"z-index": 1000 - i,
-					"-webkit-transform": "translateX(" + (-i * o.step * width - width / 2) + "px) translateZ(-200px)  rotateY(45deg)" 
-				});
-				$lis.eq(rightIndex).addClass("js-carousel-li-visible").css({
-					"z-index": 1000 - i,
-					"-webkit-transform": "translateX(" + (i * o.step * width + width / 2) + "px) translateZ(-200px) rotateY(-45deg)"
-				});
+				step:0.5,
+				rotateY:0,
+				translateZ:100,
+				preventTouchEvent:false,
+				style:"double",
+				resizeEvent: false
+			}, opt);
+		return $(this).each(function () {
+			var 
+				$this = $(this),
+				$carousel = $this,
+				$lis = $(o.li, $this),
+				lisHeight = $lis.height(),
+				lisLength = $lis.length,
+				curIndex = o.startIndex,
+				width = $carousel.width(),
+				visibleCount = o.visibleCount;
+			if (visibleCount > lisLength) {visibleCount = lisLength;}
+			function fixIndex(index) {
+				return (index + lisLength) % lisLength;
 			}
-			
-			curIndex = index;
-		}	
-		function updateSize() {			
-			width = $carousel.width();
-			translate(curIndex);
-			lisHeight = $lis.filter(".js-carousel-li-cur").height();	
-			$carousel.height(lisHeight);			
-		}
-		function init() {
-			$lis.addClass("js-carousel-li");						
-						
-			//绑定事件
-			$carousel.swipeLeft(function (e) {
-				translate(curIndex + 1);
-			});
-			$carousel.swipeRight(function (e) {
-				translate(curIndex - 1);
-			});	
-			$(window).on("resize", function (e) {
-				updateSize();
-			});	
-			$(window).on("load", function (e) {
-				updateSize();
-			});	
-			$carousel.on("touchstart", function (e) {
-		        e.preventDefault();
-			});
-			$carousel.on("touchmove", function (e) {
-		        e.preventDefault();
-			});	
-		}
-		
-		init();
-		return this;
+			function translate(index) {
+				var
+					len = visibleCount / 2,
+					i = 1,
+					leftIndex = 0,
+					rightIndex = 0;
+				index = fixIndex(index);
+				$lis.removeClass("js-carousel-li-visible").eq(index).addClass("js-carousel-li-visible js-carousel-li-cur")
+					.css({
+						"-webkit-transform": "none",
+						"z-index": 1000
+					});
+				if (o.style === "double") {
+					for(i; i <= len; i++) {
+						leftIndex = fixIndex(index - i);
+						rightIndex = fixIndex(index + i);
+						$lis.eq(leftIndex).addClass("js-carousel-li-visible").css({
+							"z-index": 1000 - i,
+							"-webkit-transform": "translateX(" + (-o.step * width * i) + "px) translateZ(-" + o.translateZ * i + "px)  rotateY(" + o.rotateY + "deg)" 
+						});
+						$lis.eq(rightIndex).addClass("js-carousel-li-visible").css({
+							"z-index": 1000 - i,
+							"-webkit-transform": "translateX(" + (o.step * width * i) + "px) translateZ(-" + o.translateZ * i + "px) rotateY(-" + o.rotateY + "deg)"
+						});
+					}
+				} else if (o.style === "left") {
+					len = visibleCount;
+					for(i; i < len; i++) {
+						rightIndex = fixIndex(index + i);
+						$lis.eq(rightIndex).addClass("js-carousel-li-visible").css({
+							"z-index": 1000 - i,
+							"-webkit-transform": "translateX(" + (o.step * width * i) + "px) translateZ(-" + o.translateZ * i + "px) rotateY(-" + o.rotateY + "deg)"
+						});
+					}
+				} else if (o.style === "right") {
+					len = visibleCount;
+					for(i; i < len; i++) {
+						leftIndex = fixIndex(index - i);
+						$lis.eq(leftIndex).addClass("js-carousel-li-visible").css({
+							"z-index": 1000 - i,
+							"-webkit-transform": "translateX(" + (-o.step * width * i) + "px) translateZ(-" + o.translateZ * i + "px) rotateY(" + o.rotateY + "deg)"
+						});
+					}
+				}
+				curIndex = index;
+			}	
+			function updateSize() {			
+				width = $carousel.width();
+				translate(curIndex);
+				lisHeight = $lis.filter(".js-carousel-li-cur").height();	
+				$carousel.height(lisHeight);			
+			}
+			function init() {
+				$lis.addClass("js-carousel-li");						
+							
+				//绑定事件
+				$carousel.swipeLeft(function (e) {
+					translate(curIndex + 1);
+				});
+				$carousel.swipeRight(function (e) {
+					translate(curIndex - 1);
+				});										
+				$(window).on("load", function (e) {
+					updateSize();
+				});	
+				if (o.resizeEvent) {
+					$(window).on("resize", function (e) {
+						updateSize();
+					});
+				}
+				if (o.preventTouchEvent) {
+					$carousel.on("touchstart", function (e) {
+				        e.preventDefault();
+					});
+					$carousel.on("touchmove", function (e) {
+				        e.preventDefault();
+					});
+				}	
+			}			
+			init();
+		});
 	};
 }(Zepto));	
